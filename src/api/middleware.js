649a0beff97155920fcecebd639f30a8b2a6f4e2
pobing.slb <pobing.slb@@alibaba-inc.com>
2023-10-25 10:14:54 +0800

    feat: git OAuth

--- src/api/middleware.js
+++ src/api/middleware.js
@@ -14,27 +14,35 @@ const limiter = rateLimit({
 /** 身份认证 */
 const apiAuth = (req, res, next) => {
   const { token } = req.cookies;
+  const { isGithub } = req.session;
+  console.log('isGithub', isGithub);
+  // TODO github Oauth token有效期
+  if (!isGithub) {
+    jwt.verify(token, JWT_SECRET_KEY, err => {
+      const isLogin = req.path === '/login';
 
-  jwt.verify(token, JWT_SECRET_KEY, err => {
-    const isLogin = req.path === '/login';
+      if (err || !token) {
+        if (!isLogin) {
+          res.redirect('/login');
+        }
 
-    if (err || !token) {
-      if (!isLogin) {
-        res.redirect('/login');
+        next();
+        // res.status(500).json({ message: 'token is inValid! please re-login', status: 0 });
+        // return;
+      } else {
+        if (isLogin) {
+          res.send('已登录');
+          return;
+        }
+
+        next();
       }
+    });
 
-      next();
-      // res.status(500).json({ message: 'token is inValid! please re-login', status: 0 });
-      // return;
-    } else {
-      if (isLogin) {
-        res.send('已登录');
-        return;
-      }
+    return;
+  }
 
-      next();
-    }
-  });
+  next();
 };
 
 /** 相同用户名的登录状态session存储数量上限(for 登录设备数量上限) */
--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 const cookieParser = require('cookie-parser');
 const app = express();
 const path = require('path');
+const qs = require('qs');
+const axios = require('axios');
+const chalk = require('chalk');
 const api = require('./api');
 const initSession = require('./session');
 const { apiAuth } = require('./api/middleware');
@@ -13,8 +16,10 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(cookieParser());
 
+const isDev = process.env.NODE_ENV == 'DEV';
+
 // 开发环境模拟网络delay
-if (process.env.NODE_ENV == 'DEV') {
+if (isDev) {
   app.use('/api', async (req, res, next) => {
     // 每个请求模拟 100ms延时
     const delay = 500;
@@ -28,6 +33,57 @@ if (process.env.NODE_ENV == 'DEV') {
 
 initSession(app).then(redisStore => {
   api(app, { redisStore });
+
+  // github OAuth callbak
+  app.get('/callback', async (req, res) => {
+    const { code } = req.query;
+
+    if (code) {
+      /* get git token */
+      const { data } = await axios
+        .post('https://github.com/login/oauth/access_token', {
+          code,
+          client_id: isDev && '5da64e99978d1a198b9d',
+          client_secret: isDev && 'e387adb9dea304393e4a975a546b09b136dfef22',
+        })
+        .catch(err => {
+          console.log(chalk.red(err));
+          res.json({ message: 'github OAuth app login error', status: 0 });
+          return {};
+        });
+
+      if (data) {
+        const { access_token } = qs.parse(data);
+        // TODO cookie保存git token 校验github 有效期
+        /* get git account info */
+        const { data: userData } = await axios('https://api.github.com/user', {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }).catch(err => {
+          console.log(chalk.red(err));
+          return {};
+        });
+        console.log('userData', userData);
+
+        if (userData) {
+          // TODO mongodb存git账户信息
+          /* 会话信息中保存git账户登录信息 */
+          req.session.username = userData.login;
+          req.session.isGithub = true;
+
+          res.redirect('/chatroom');
+          return;
+        }
+        // git token失效
+        res.redirect('/login');
+      }
+
+      return;
+    }
+
+    res.redirect('/login');
+  });
 
   app.get('*', apiAuth, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/main.html'));
